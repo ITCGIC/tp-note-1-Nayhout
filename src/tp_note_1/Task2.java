682aff4ef503a9eac473fed0e456d950018cef56
Nayhout <yem.nayhout@gmail.com>
2019-12-20 14:49:47 +0700

    Task2

---
+++ src/tp_note_1/Task2.java
@@ -0,0 +1,24 @@
+package tp_note_1;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+        int seconds;
+        int minutes ;
+        int hours;
+        System.out.print("Input number of seconds: ");
+        seconds = input.nextInt();
+        hours = seconds / 3600;
+        minutes = (seconds%3600)/60;
+        int seconds_output = (seconds% 3600)%60;
+
+
+        System.out.printf("Time corresponding to "+seconds+ " seconds is ");
+        System.out.println(hours  + ":" + minutes + ":" + seconds_output);
+    }
+
+}
